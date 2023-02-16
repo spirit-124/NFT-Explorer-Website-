d2c9603dfe82996ef58381e020569adf8d07e711
spirit-124 <bajrangofficial580@gmail.com>
2023-02-17 00:07:02 +0530

    [Added] Smart Contract

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,42 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+// const hre = require("hardhat");
+
+// async function main() {
+  const main = async()=>{
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  // const lockedAmount = hre.ethers.utils.parseEther("1");
+
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const transactions = await Transactions.deploy()
+
+  await transactions.deployed();
+
+  console.log(
+    `Transaction deployed to: ${transactions.address}`
+  );
+}
+
+
+
+const runMain = async() => {
+  try {
+    await main();
+    process.exit(0);
+  }
+  catch (err) {
+    console.error(err);
+    process.exit(1);
+  
+}
+}
+
+runMain();
