003604cfd12dd71bf80411bd3a5480a614a15bb9
spirit-124 <bajrangofficial580@gmail.com>
2023-02-22 21:36:46 +0530

    Minor Changes

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,6 @@
-// export { default as Footer } from "./Footer";
-// export { default as Navbar } from "./Navbar";
-// export { default as Transaction } from "./Transaction";
-// export { default as Loader } from "./Loader";
-// export { default as Welcome } from "./Welcome";
-// export { default as Services } from "./Services";
+export { default as Footer } from "./Footer";
+export { default as Navbar } from "./Navbar";
+export { default as Transaction } from "./Transactions";
+export { default as Loader } from "./Loader";
+export { default as Welcome } from "./Welcome";
+export { default as Services } from "./Services";
